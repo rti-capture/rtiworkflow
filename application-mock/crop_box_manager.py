e0bccea4a0018b77f6b832388a746fabc0a2bca4
Benjamin Thompson <bt1g18@soton.ac.uk>
2020-06-24 14:01:18 +0100

    Added mins and max for each cropline

--- application-mock/crop_box_manager.py
+++ application-mock/crop_box_manager.py
@@ -1,3 +1,8 @@
+'''
+Tasks:
+get new dimensions of image
+'''
+
 from crop_line import *
 
 class CropBoxManager():
@@ -11,10 +16,18 @@ class CropBoxManager():
         self.menubaroffset = menubaroffset
         self.croplineoffset = 3
 
-        self.croplineL = CropLine(parent, True, self.labelx, self.labely + menubaroffset, self.labelheight, self.croplineoffset)
-        self.croplineR = CropLine(parent, True, self.labelx + self.labelwidth - self.croplineoffset, self.labely + menubaroffset, self.labelheight, self.croplineoffset)
-        self.croplineT = CropLine(parent, False, self.labelx, self.labely + menubaroffset, self.labelwidth, self.croplineoffset)
-        self.croplineB = CropLine(parent, False, self.labelx, self.labely + self.labelheight + menubaroffset - self.croplineoffset, self.labelwidth, self.croplineoffset)
+        #min and max values for croplines
+        self.minx = self.labelx
+        self.midx = self.labelx + self.labelwidth / 2
+        self.maxx = self.labelx + self.labelwidth - self.croplineoffset
+        self.miny = self.labely + self.menubaroffset
+        self.midy = self.labely + self.menubaroffset + self.labelheight / 2
+        self.maxy = self.labely + self.menubaroffset + self.labelheight - self.croplineoffset
+
+        self.croplineL = CropLine(parent, "L", self.labelx, self.labely + menubaroffset, self.labelheight, self.croplineoffset)
+        self.croplineR = CropLine(parent, "R", self.labelx + self.labelwidth - self.croplineoffset, self.labely + menubaroffset, self.labelheight, self.croplineoffset)
+        self.croplineT = CropLine(parent, "T", self.labelx, self.labely + menubaroffset, self.labelwidth, self.croplineoffset)
+        self.croplineB = CropLine(parent, "B", self.labelx, self.labely + self.labelheight + menubaroffset - self.croplineoffset, self.labelwidth, self.croplineoffset)
 
         self.croplineL.bind("<Button-1>", self.on_start)
         self.croplineL.bind("<B1-Motion>", self.on_drag)
@@ -30,6 +43,14 @@ class CropBoxManager():
         changey = self.labely - self.label.winfo_y()
         self.labelx = self.label.winfo_x()
         self.labely = self.label.winfo_y()
+
+        self.minx = self.labelx
+        self.midx = self.labelx + self.labelwidth / 2
+        self.maxx = self.labelx + self.labelwidth - self.croplineoffset
+        self.miny = self.labely + self.menubaroffset
+        self.midy = self.labely + self.menubaroffset + self.labelheight / 2
+        self.maxy = self.labely + self.menubaroffset + self.labelheight - self.croplineoffset
+
         self.croplineR.place(x=(self.croplineR.winfo_x() - changex), y=(self.croplineR.winfo_y() - changey))
         self.croplineL.place(x=(self.croplineL.winfo_x() - changex), y=(self.croplineL.winfo_y() - changey))
         self.croplineT.place(x=(self.croplineT.winfo_x() - changex), y=(self.croplineT.winfo_y() - changey))
@@ -42,18 +63,24 @@ class CropBoxManager():
 
     def on_drag(self, event):
         widget = event.widget
-        """
-        if x > 202:
-            x = 202
-        elif x < 0:
-            x = 0
-        print(x)
-        """
-        if (widget.isVertical):
+        if widget.position == "L":
             x = widget.winfo_x() - widget._drag_start_x + event.x
+            x = x if x >= self.minx else self.minx
+            x = x if x <= self.midx - 2 else self.midx - 2
             y = widget.winfo_y()
+        elif widget.position == "R":
+            x = widget.winfo_x() - widget._drag_start_x + event.x
+            x = x if x >= self.midx + self.croplineoffset + 2 else self.midx + self.croplineoffset + 2
+            x = x if x <= self.maxx else self.maxx
+            y = widget.winfo_y()
+        elif widget.position == "T":
+            x = widget.winfo_x()
+            y = widget.winfo_y() - widget._drag_start_y + event.y
+            y = y if y >= self.miny else self.miny
+            y = y if y <= self.midy - 2 else self.midy - 2
         else:
             x = widget.winfo_x()
             y = widget.winfo_y() - widget._drag_start_y + event.y
-        print(y)
+            y = y if y >= self.midy + self.croplineoffset + 2 else self.midy + self.croplineoffset + 2
+            y = y if y <= self.maxy else self.maxy
         widget.place(x=x, y=y)
--- application-mock/crop_line.py
+++ application-mock/crop_line.py
@@ -1,12 +1,12 @@
 from tkinter import *
 
 class CropLine(Canvas):
-    def __init__(self, parent, isVertial, x, y, length, croplineoffset):
-        if isVertial:
+    def __init__(self, parent, position, x, y, length, croplineoffset):
+        if position == "L" or position == "R":
             Canvas.__init__(self, parent, background="red", width=croplineoffset, height=length, highlightthickness=0)
         else:
             Canvas.__init__(self, parent, background="red", width=length, height=croplineoffset, highlightthickness=0)
-        self.isVertical = isVertial
+        self.position = position
         self.place(x=x, y=y)
 
 
